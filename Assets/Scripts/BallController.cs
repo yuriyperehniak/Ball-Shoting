3eaec66d1bf4d8000e711eaabbb9c4cfb1d52a32
Yuriy Perehniak <yuraperehniak@gmail.com>
2023-09-26 12:27:05 +0300

    fix ballController script, add some changes in scene

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,57 +2,84 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public float spawnSpeed = 10f;
-    public float moveSpeed = 70f;
+    public float spawnSpeed = 5f;  
+    public float moveSpeed = 20f;
     public GameObject objectPrefab;
-
-    private Transform _sizeOriginal;
-    private Transform _sizeClone;
+    
+    private Transform _originalTransform;
+    private Transform _cloneTransform;
     private GameObject _clone;
+    private Camera _mainCamera;
 
+    private Vector3 _originalScale;
+    private Vector3 _cloneScale;
     private bool _isTouching;
-    private Transform _transform;
-    private Camera _camera;
-    private bool _isCameraNotNull;
-    private Transform _transform1;
-    private bool _isCloneNotNull;
+    private Rigidbody _rigidbody;
+    private bool _isRigidbodyNotNull;
+    private bool _isCloneNull;
+    private bool _isMainCameraNull;
 
     private void Start()
     {
-        _isCloneNotNull = _clone != null;
-        _transform1 = _clone.GetComponent<Transform>();
-        var main = Camera.main;
-        _isCameraNotNull = main != null;
-        _camera = main;
-        _transform = gameObject.GetComponent<Transform>();
-        _sizeOriginal = _transform;
+        _isMainCameraNull = _mainCamera == null;
+        _isCloneNull = _clone == null;
+        _mainCamera = Camera.main;
+        _originalTransform = transform;
+
+        _originalScale = _originalTransform.localScale;
     }
 
     private void Update()
     {
         if (Input.touchCount > 0)
         {
-            foreach (var touch in Input.touches)
-            {
-                if (touch.phase != TouchPhase.Began) continue;
-                if (_isTouching || !_isCameraNotNull) continue;
-                var worldPosition = _camera.ScreenToWorldPoint(touch.position);
+            var touch = Input.GetTouch(0);
 
-                if (_isCloneNotNull) continue;
-                _clone = Instantiate(objectPrefab, worldPosition, Quaternion.identity);
-                _sizeClone = _transform1;
+            if (touch.phase != TouchPhase.Began || _isMainCameraNull)
+            {
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    _isTouching = false;
+                    ShootingBall();
+                }
+            }
+            else
+            {
+                if (_isCloneNull)
+                {
+                    // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+                    var cloneRenderer = objectPrefab.GetComponent<Renderer>();
+                    var cloneHeight = cloneRenderer.bounds.size.y;
+
+                    var yPos = cloneHeight * 0.5f;
+
+                    _clone = Instantiate(objectPrefab, new Vector3(0, yPos, 5f), Quaternion.identity);
+                    // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+                    _cloneTransform = _clone.GetComponent<Transform>();
+                    _cloneScale = Vector3.zero;
+                    // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+                    _rigidbody = _clone.GetComponent<Rigidbody>();
+                    // ReSharper disable once Unity.PerformanceCriticalCodeNullComparison
+                    _isRigidbodyNotNull = _rigidbody != null;
+                }
+
                 _isTouching = true;
             }
         }
 
         if (!_isTouching) return;
-        var growthRate = Time.deltaTime * spawnSpeed;
-        _sizeClone.localScale += new Vector3(growthRate, growthRate, growthRate);
-        _sizeOriginal.localScale -= new Vector3(growthRate, growthRate, growthRate);
+        _cloneScale += new Vector3(spawnSpeed * Time.deltaTime, spawnSpeed * Time.deltaTime, spawnSpeed * Time.deltaTime);
+        _cloneTransform.localScale = _cloneScale;
+
+        _originalScale -= new Vector3(spawnSpeed * Time.deltaTime, spawnSpeed * Time.deltaTime, spawnSpeed * Time.deltaTime);
+        _originalTransform.localScale = _originalScale;
     }
 
-    public void OnTouchUp()
+    private void ShootingBall()
     {
-        _isTouching = false;
+        if (_isRigidbodyNotNull)
+        {
+            _rigidbody.AddForce(0, 0, moveSpeed, ForceMode.Impulse);
+        }
     }
 }
